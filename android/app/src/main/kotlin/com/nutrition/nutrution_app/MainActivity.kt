9dcee69dc9534e70f5e1ec345a73dd82889d68fd
Ibrohimov-Abdullah <abdullloibrohimov3@gmail.com>
2025-10-29 16:38:47 +0500

    done

---
+++ android/app/src/main/kotlin/com/nutrition/nutrution_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.nutrition.nutrution_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+// src - barcha papka va oynlar, dizayn turadigon joy
+
+
+// start - loyihani ishga churish uchun void main birinchi ishlovchi narsalar uchun
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:nutrution_app/src/core/style/app_colors.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: Scaffold(
+        backgroundColor: AppColors.black,
+      ),
+    );
+  }
+}
