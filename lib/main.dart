670fd88138441d55a5b94e1a8f698c8289149758
sevinchtursunova065-cmyk <sevinchtursunova065@gmail.com>
2025-11-01 10:51:43 +0500

    5, sevinch text_style.dart bosh joy tashadi

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,20 @@
-// src - barcha papka va oynlar, dizayn turadigon joy
-
-
-// start - loyihani ishga churish uchun void main birinchi ishlovchi narsalar uchun
-
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:nutrution_app/src/core/style/app_colors.dart';
+
+
+import 'assasiy.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
+
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      home: Scaffold(
-        backgroundColor: AppColors.black,
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Asosiy_oyna(),
     );
   }
 }
